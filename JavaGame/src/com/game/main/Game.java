b884147d401106db56c333d3fbf72e482971caa7
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2019-11-08 11:43:08 -0500

    deleted some enemy objects

--- JavaGame/src/com/game/main/Game.java
+++ JavaGame/src/com/game/main/Game.java
@@ -32,10 +32,7 @@ public class Game extends Canvas implements Runnable {
 
         handler.addObject(new Player(WIDTH/2 -32, HEIGHT/2 -32, ID.Player, handler));
         handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
-        handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
-        handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
-        handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
-        handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
+        
     }
 
     public synchronized void start() {
